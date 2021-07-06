56b50ef2c5a3e12eb72a4fc648711d5e4aca55a2
Joshua Coleman <coleman@hey.com>
2021-07-06 11:47:05 -0500

    Install package.json and gulp task runner packages.

---
+++ gulpfile.js
@@ -0,0 +1,112 @@
+/*------------------------------
+ Gulp
+------------------------------*/
+
+// Task runners to use for the new newsletter templates for Crosspoint Church.
+
+/* Plugins
+------------------------------*/
+
+// Notes...
+
+var gulp =		  require('gulp');
+var inlinecss =   require('gulp-inline-css');
+var imagemin =	  require('gulp-imagemin');
+var browsersync = require('browser-sync');
+
+/* Directories
+------------------------------*/
+
+// Notes...
+
+var basePaths = {
+
+	dev:	   'src/',
+	dist:	   'dist/html/',
+	new:	   'new/',
+	originals: 'originals/',
+	assets:	   'dist/html/assets/'
+
+};
+
+/* Inline CSS
+------------------------------*/
+
+// Notes...
+
+gulp.task('inlinecss', function() {
+
+	return gulp.src(basePaths.new + '*.html')
+		.pipeline(inlinecss())
+		.pipe(gulp.dest(basePaths.dist));
+
+});
+
+/* Images
+------------------------------*/
+
+// Notes...
+
+gulp.task('img', function() {
+
+	return gulp.src(basePaths.dev + 'img/**/*')
+
+	.pipe(imagemin({
+
+		optimizationLevel: 5,
+		progressive: true,
+		interlaced: true
+
+	}))
+
+	.pipe(gulp.dest(basePaths.assets + 'img'));
+
+});
+
+/* Fonts
+------------------------------*/
+
+// Notes...
+
+gulp.task('fonts', function() {
+
+	return gulp.src(basePaths.dev + 'fonts/**/*')
+
+	.pipe(gulp.dest(basePaths.assets + 'fonts'));
+
+});
+
+/* Watch
+------------------------------*/
+
+// Notes...
+
+gulp.task('watch', function() {
+
+	browsersync.init({
+
+		proxy: "https://newsletters.local",
+		host: "newsletters.local",
+		open: "external",
+		// browser: 'microsoft edge'
+
+	});
+
+	gulp.watch(basePaths.dev + 'scss/**/*.scss', gulp.series('sass'));
+
+	// gulp.watch(basePaths.dev + 'js/*.js', gulp.series('js'));
+
+	gulp.watch(basePaths.dev + 'img/**/*', gulp.series('img'));
+
+	// gulp.watch(basePaths.dev + 'fonts/**/*', gulp.series('fonts'));
+
+	gulp.watch(basePaths.dist + '**/*.html').on('change', browsersync.reload);
+
+});
+
+/* Title
+------------------------------*/
+
+// Notes...
+
+gulp.task('default', gulp.parallel('inlinecss', 'img', 'fonts', 'watch'));
